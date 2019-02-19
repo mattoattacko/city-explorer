66fed83876290ad2b0850b6e7657910c796cbabc
Matto <matt.mcquain@gmail.com>
2019-02-18 21:32:17 -0800

    styling edit

--- city-explorer/src/components/form.js
+++ city-explorer/src/components/form.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Form(props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <label htmlFor="city">Search for a location</label>
+      <label htmlFor="city">Where Would You Like To Learn About?</label><br></br>
       <input
         type="text"
         name="city"
         onChange={props.handleChange}
-        placeholder="Enter a location here"
+        placeholder="Please Enter Location"
       />
       <button type="submit">Lets Explore!</button>
     </form>
